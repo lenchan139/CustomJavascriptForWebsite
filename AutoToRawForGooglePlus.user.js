8facffa73ae0c5bc7e96bd58f2f480aca61b0bcb
lenchan139 <lenchan139@users.noreply.github.com>
2017-02-04 22:06:13 +0800

    work with some gif.

--- AutoToRawForGooglePlus.user.js
+++ AutoToRawForGooglePlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto to Raw for Google+ Image
 // @namespace    http://lenchan139.org/
-// @version      1.0
+// @version      1.1
 // @description  Auto to raw for Google+ Images.
 // @author       lenchan139
 // @match        https://*.googleusercontent.com/*
@@ -11,11 +11,18 @@
 (function() {
     'use strict';
 
-   var t=window.location.href.search(/\/\w\d+(-\w\d*)-p-rw\//);
-if(t==-1){
+    var short=window.location.href.search(/\/\w\d+(-\w\d*)-rw\//);
+    
+    var long=window.location.href.search(/\/\w\d+(-\w\d*)-p-rw\//);
+    
+    var header=window.location.href.search(/https:\/\/lh[0-9]+.googleusercontent.com/g);
+    
+if(short!=-1 && header!=-1){
+  window.location.href=window.location.href.replace(/\/\w\d+(-\w\d*)-rw\//, '/s0/');
+}else if(long!=-1 && header!=-1 ){
 
+  window.location.href=window.location.href.replace(/\/\w\d+(-\w\d*)-p-rw\//, '/s0/');
 }else{
-    window.location.href=window.location.href.replace(/\/\w\d+(-\w\d*)-p-rw\//, '/s0/');
 }
 
 })();
