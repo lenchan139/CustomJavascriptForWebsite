b8bb913e6046a741d27e9d63906096c62dc40094
lenchan139 <lenchan139@users.noreply.github.com>
2017-04-26 22:42:41 +0800

    Create MastodonScaler.user.js

---
+++ MastodonScaler.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Page Scaler For Mastodon
+// @namespace    https://lenchan139.org/
+// @version      0.1
+// @description  Remember change the (at)match for your Mastodon domain.
+// @author       You
+// @match        https://sns.lenchan139.org/
+// @match        https://sns.lenchan139.org/web/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.body.style.zoom=0.95;
+  
+})();
