68231f8c725df373df631d6519eb8d1e22f0ed2d
lenchan139 <lenchan139@users.noreply.github.com>
2017-07-13 23:47:05 +0800

    Update AutoToRawforTwitterImage.user.js

--- AutoToRawforTwitterImage.user.js
+++ AutoToRawforTwitterImage.user.js
@@ -4,14 +4,25 @@
 // @version      1.1
 // @description  Auto to raw for Twitter Images.
 // @author       lenchan139
-// @match        https://pbs.twimg.com/*
+// @match        https://pbs.twimg.com/media/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    autoToRaw();
+})();
+ 
+ function autoToRaw() {
+
     'use strict';
 
-    var header=window.location.href.search(/\/media\/\w+.\w\w\w/g);
+    var pattern1=window.location.href.search(/\/media\/\w+\.\w\w\w/);
+    var pattern2a=window.location.href.search(/\?\w+\=\w+\&\w+\=\w+/g);
+    var pattern2b=window.location.href.search(/\?\w+\=\w+/g);
+    var finder1=window.location.href.search(/\?/g);
+    var finder2=window.location.href.search(/\&/g);
+    var finder3=window.location.href.search(/\:\w+/g);
+    var finder4=window.location.href.search(/name=orig/g);
     
     var long1=window.location.href.search(/:small/);
     
@@ -20,6 +31,48 @@
     var long3=window.location.href.search(/:orig/);
     
     
+    //alert(pattern1 + "|" + finder1 + "|" + finder2 + "|" + finder3);
+    
+  
+    //start
+    //if correct, do nothing
+     alert(pattern1 + "|" + pattern2a +"|" + pattern2b + "|" + finder1 + "|" + finder2 + "|" + finder3);
+     if(pattern1==-1 && pattern2a!=-1 && pattern2b!=-1 && finder1!=-1 && finder2!=-1 && finder4!=-1){
+         //do nothing
+         alert("good!");
+     }else if(pattern1!=-1 && finder1==-1 && finder2==-1 && finder3==-1){
+        var arr = window.location.href.match(/\/media\/\w+\.\w\w\w/);
+        var str1 = arr[0];
+        var arr2 = str1.match(/\.\w\w\w/);
+        var str2 = arr2[0];
+        window.location.href=window.location.href.replace(str2,"?format="+str2.substring(1)+"&name=orig");
+        
+    }else if(pattern1!=-1 && finder1==-1 && finder2==-1 && finder3!=-1){
+        
+        var arr1 = window.location.href.match(/\/media\/\w+.\w\w\w\:\w+/);
+        var str1 = arr1[0];
+        alert(str1);
+        //format
+        var arr2 = str1.match(/\.\w\w\w/);
+        var str2 = arr2[0];
+        //size
+        var arr3 = str1.match(/\:\w+/g);
+        var str3 = arr3[0];
+        var url = window.location.href;
+        url=url.replace(str2,"?format="+str2.substring(1));
+        url=url.replace(str3,"&name=orig");
+        window.location.href = url;
+    }else if(pattern2a!=-1 && pattern2b!=-1 && finder3==-1){
+        window.location.href=window.location.href.replace(/name=\w+/g,"name=orig");   
+    }else if(pattern2a==-1 && pattern2b!=-1 && finder3==-1){
+        window.location.href=window.location.href + "&name=orig";  
+    }
+    
+    
+    
+    
+   //old
+    /*
 if(header!=-1 && long3!=-1){
   
 }else if(long2!=-1 && header!=-1 ){
@@ -32,5 +85,5 @@ if(header!=-1 && long3!=-1){
     
   window.location.href=window.location.href + ':orig';
 }
-
-})();
+*/
+ }
